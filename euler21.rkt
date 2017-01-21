b989fc339b52127054d8637e3ba6247944f62c09
kronicmage <kronicmage@gmail.com>
2017-01-21 16:19:51 -0500

    Replaced listsum with foldl

--- euler21.rkt
+++ euler21.rkt
@@ -5,14 +5,10 @@
     [(= counter (sqrt x)) (append lst (list counter))]
     [(= (modulo x counter) 0) (badfactor x (append lst (list counter (/ x counter))) (add1 counter))]
     [else (badfactor x lst (add1 counter))]))
-(define (listsum lst [n 0])
-  (cond
-    [(empty? lst) n]
-    [else (listsum (cdr lst) (+ n (car lst)))]))
 (define (euler21 [x 1] [lst empty])
   (cond
-    [(>= x 10000) (listsum lst)]
-    [(and (= x (listsum (badfactor (listsum (badfactor x))))) (not (= x (listsum (badfactor x)))))
+    [(>= x 10000) (foldl + 0 lst)]
+    [(and (= x (foldl + 0 (badfactor (foldl + 0 (badfactor x))))) (not (= x (foldl + 0 (badfactor x)))))
      (euler21 (add1 x) (append lst (list x)))]
     [else (euler21 (add1 x) lst)]))
 (euler21)
